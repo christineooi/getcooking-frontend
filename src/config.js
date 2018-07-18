f38902273557d7500dbe474e9e43bbf8c9433003
Christine Ooi <christine.ooi@gmail.com>
2018-07-17 21:34:14 -0400

    fixed backend url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const backendurl= 'https://getcooking-backend.herokuapp.com/';
+export const backendurl= 'https://getcooking-backend.herokuapp.com';
 // export const backendurl='http://localhost:3000';
