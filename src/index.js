08e0255f4c4aec7753ee84a99f383b2072529f35
Christine Ooi <christine.ooi@gmail.com>
2018-07-22 10:29:54 -0400

    Modified index.js to use Redux compose if redux devtool extension is not installed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers/index.js';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-    reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    reducer, composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
